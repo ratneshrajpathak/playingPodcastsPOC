880d39e75f0889b93eb192618b4f6a8a6a85c7aa
ratneshrajpathak <123545063+ratneshrajpathak@users.noreply.github.com>
2023-08-28 12:47:13 +0530

    Delete ProjetoGuilda:PlayAudio directory
    Remove

--- ProjetoGuilda:PlayAudio/Pods/Target Support Files/Pods-ProjetoGuilda/Pods-ProjetoGuilda-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjetoGuilda : NSObject
-@end
-@implementation PodsDummy_Pods_ProjetoGuilda
-@end
--- ProjetoGuilda:PlayAudio/Pods/Target Support Files/Pods-ProjetoGuildaTests/Pods-ProjetoGuildaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjetoGuildaTests : NSObject
-@end
-@implementation PodsDummy_Pods_ProjetoGuildaTests
-@end
